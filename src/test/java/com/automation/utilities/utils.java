74996d84a4a952e7fb721dd1139c2283ee055f02
parvin <pkouliev@gmail.com>
2021-02-16 23:36:27 -0500

    utils class added

---
+++ src/test/java/com/automation/utilities/utils.java
@@ -0,0 +1,8 @@
+package com.automation.utilities;
+
+public class utils {
+
+    public static void space() {
+        System.out.println("\n########################################################################################\n");
+    }
+}
